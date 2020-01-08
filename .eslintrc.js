5cf8b98dc656602db0f67e3e7fc6364639b77b92
Marian <marianmrva123@gmail.com>
2020-01-08 01:57:24 +0100

    Comment eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
-// https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb
-module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  extends: [
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+// // https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb
+// module.exports = {
+//   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+//   extends: [
+//     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+//     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+//     'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
 
-  ],
-  parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true, // Allows for the parsing of JSX
-    },
-  },
-  rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+//   ],
+//   parserOptions: {
+//     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+//     sourceType: 'module', // Allows for the use of imports
+//     ecmaFeatures: {
+//       jsx: true, // Allows for the parsing of JSX
+//     },
+//   },
+//   rules: {
+//     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+//     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     
-  },
-  settings: {
-    react: {
-      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
-  },
-};
+//   },
+//   settings: {
+//     react: {
+//       version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+//     },
+//   },
+// };
