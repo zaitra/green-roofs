48c9529f364ccc91ecc4ba89f5d9d3adf97166a6
surfer19 <marianmrva123@gmail.com>
2020-01-19 15:46:03 +0000

    Deploying to gh-pages from master d570b2d1ca29cf67bdf995a5f0957bc3396636a9

---
+++ precache-manifest.7846f0142a6155e81095da8977e2d5b4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35ffb4e8b490db5161c3c630c71581c",
+    "url": "https://surfer19.github.io/green-roofs/index.html"
+  },
+  {
+    "revision": "0f6516a4e181feb599e3",
+    "url": "https://surfer19.github.io/green-roofs/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "4ebc8bff07331d1045c7",
+    "url": "https://surfer19.github.io/green-roofs/static/css/main.fed6b617.chunk.css"
+  },
+  {
+    "revision": "0f6516a4e181feb599e3",
+    "url": "https://surfer19.github.io/green-roofs/static/js/2.1f283af6.chunk.js"
+  },
+  {
+    "revision": "ce3126c332c3bc8f1544c88c174f7c51",
+    "url": "https://surfer19.github.io/green-roofs/static/js/2.1f283af6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ebc8bff07331d1045c7",
+    "url": "https://surfer19.github.io/green-roofs/static/js/main.eef1e916.chunk.js"
+  },
+  {
+    "revision": "923a77417444b5ae9840",
+    "url": "https://surfer19.github.io/green-roofs/static/js/runtime-main.e52a5657.js"
+  },
+  {
+    "revision": "b943d45302950686a84bd78c32cccdaf",
+    "url": "https://surfer19.github.io/green-roofs/static/media/green-building-blur.b943d453.jpg"
+  },
+  {
+    "revision": "8a4c86e8f1064591eafe0911a311bb07",
+    "url": "https://surfer19.github.io/green-roofs/static/media/green-building-top.8a4c86e8.jpg"
+  },
+  {
+    "revision": "41eb8d90eec45409b241b8c0dcefb537",
+    "url": "https://surfer19.github.io/green-roofs/static/media/green-roofs-logo-leaf.41eb8d90.svg"
+  },
+  {
+    "revision": "9d3a34148ef426759a7e6422cccbc434",
+    "url": "https://surfer19.github.io/green-roofs/static/media/green-roofs-logo.9d3a3414.svg"
+  },
+  {
+    "revision": "dc9cf7d9bc465eb50983846b4fca49d1",
+    "url": "https://surfer19.github.io/green-roofs/static/media/heatmap.dc9cf7d9.jpg"
+  },
+  {
+    "revision": "34b5f337fdb6688a987e9716f82457d2",
+    "url": "https://surfer19.github.io/green-roofs/static/media/zaitra-logo.34b5f337.svg"
+  }
+]);
