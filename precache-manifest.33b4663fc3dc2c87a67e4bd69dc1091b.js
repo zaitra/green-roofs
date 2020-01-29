1f25d11171214a6a03596e8d5425f19e0473cd77
surfer19 <marianmrva123@gmail.com>
2020-01-29 15:20:26 +0100

    Updates

---
+++ precache-manifest.33b4663fc3dc2c87a67e4bd69dc1091b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ab4ab5755cfabc99f5d56c31540225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45fdef22840e72fdf693",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "4f7d671ecec4d9920fe8",
+    "url": "/static/css/main.1462f6a3.chunk.css"
+  },
+  {
+    "revision": "45fdef22840e72fdf693",
+    "url": "/static/js/2.3166ebc9.chunk.js"
+  },
+  {
+    "revision": "a9b97762f6d6e85e5da3d8b0ec561f4a",
+    "url": "/static/js/2.3166ebc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f7d671ecec4d9920fe8",
+    "url": "/static/js/main.f061f9f7.chunk.js"
+  },
+  {
+    "revision": "a88e71ce873efefe0523",
+    "url": "/static/js/runtime-main.f6dd98db.js"
+  },
+  {
+    "revision": "b943d45302950686a84bd78c32cccdaf",
+    "url": "/static/media/green-building-blur.b943d453.jpg"
+  },
+  {
+    "revision": "8a4c86e8f1064591eafe0911a311bb07",
+    "url": "/static/media/green-building-top.8a4c86e8.jpg"
+  },
+  {
+    "revision": "41eb8d90eec45409b241b8c0dcefb537",
+    "url": "/static/media/green-roofs-logo-leaf.41eb8d90.svg"
+  },
+  {
+    "revision": "9d3a34148ef426759a7e6422cccbc434",
+    "url": "/static/media/green-roofs-logo.9d3a3414.svg"
+  },
+  {
+    "revision": "4cc581d2ed254dae72553a343f0e1694",
+    "url": "/static/media/heatmap-after.4cc581d2.jpg"
+  },
+  {
+    "revision": "dc9cf7d9bc465eb50983846b4fca49d1",
+    "url": "/static/media/heatmap.dc9cf7d9.jpg"
+  },
+  {
+    "revision": "7c30ca3928ccb33b9b4097c66641869a",
+    "url": "/static/media/mapIcon.7c30ca39.svg"
+  },
+  {
+    "revision": "34b5f337fdb6688a987e9716f82457d2",
+    "url": "/static/media/zaitra-logo.34b5f337.svg"
+  }
+]);
